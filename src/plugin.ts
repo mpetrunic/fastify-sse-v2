3f08df24a0bfd271dfe5eae65c20386e01210d1c
Marin Petrunić <marin.petrunic@gmail.com>
2020-07-20 16:44:25 +0200

    refactor for fastify v3

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,18 +1,19 @@
-import {EventMessage, FastifyReply, Plugin} from "fastify";
-import {IncomingMessage, Server, ServerResponse} from "http";
+import {EventMessage, FastifyPluginAsync, FastifyReply} from "fastify";
 import {SsePluginOptions} from "./types";
+import {Writable} from "stream";
 import {serializeSSEEvent, transformAsyncIterable} from "./sse";
 import toStream from "it-to-stream";
 
-export const plugin: Plugin<Server, IncomingMessage, ServerResponse, SsePluginOptions> =
+export const plugin: FastifyPluginAsync<SsePluginOptions> =
     async function (instance, options): Promise<void> {
       instance.decorateReply(
         "sse",
-        function (this: FastifyReply<ServerResponse>, source: AsyncIterable<EventMessage>): void {
-          this.res.write(serializeSSEEvent({retry: options.retryDelay || 3000}));
+        function (this: FastifyReply, source: AsyncIterable<EventMessage>): void {
+          const outputStream: Writable = this.raw;
+          outputStream.write(serializeSSEEvent({retry: options.retryDelay || 3000}));
           this.type("text/event-stream")
             .header("Connection", "keep-alive")
             .header("Cache-Control", "no-cache");
-          toStream(transformAsyncIterable(source)).pipe(this.res);
+          toStream(transformAsyncIterable(source)).pipe(outputStream);
         });
     };
